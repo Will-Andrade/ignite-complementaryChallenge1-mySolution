b9b6a2e96e21dfa67db1dd88e43a714db83be7d7
Will-Andrade <willian.santos.andra@outlook.com>
2022-02-14 14:09:05 -0400

    feat: creates the SideBar component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,31 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+
+interface SideBarProps {
+  genreId: number;
+  genres: Array<{
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }>
+  handleGenreClick: (id: number) => void;
+}
+
+export function SideBar({ genreId, genres, handleGenreClick }: SideBarProps) {
+  return (
+  <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => handleGenreClick(genre.id)}
+          selected={genreId === genre.id}
+        />
+      ))}
+    </div>
+  </nav>
+  );
 }
